cee010c927068555ac0c29953a3df5ff54d1c08e
ichigayamate <kyokodenshae353kei@gmail.com>
2024-06-15 21:53:17 +0700

    solved sonarlint error in layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
