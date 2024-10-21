b36ba407db83add53fb46b8012b7e5aa436b80ae
ichigayamate <kyokodenshae353kei@gmail.com>
2024-10-21 10:28:14 +0700

    seo/added seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,13 +18,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://i10e.dev" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content={metadata.title} />
+
+        <link rel="icon" href="/manifest/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          href="/manifest/apple-touch-icon.png"
+          sizes="76x76"
+        />
+        <link rel="manifest" href="/manifest/manifest.webmanifest" />
+      </head>
       <body>
         <ThemeProvider attribute="class">
           <noscript>You need to enable JavaScript to run this app</noscript>
           <div id="root">
-            <Layout>
-              {children}
-            </Layout>
+            <Layout>{children}</Layout>
           </div>
         </ThemeProvider>
       </body>
