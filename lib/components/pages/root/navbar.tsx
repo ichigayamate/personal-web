aea6ab6049275517ffc0ce77750ec1a73f933509
ichigayamate <kyokodenshae353kei@gmail.com>
2024-10-29 11:45:31 +0700

    main/added navbar

--- lib/components/pages/root/navbar.tsx
+++ lib/components/pages/root/navbar.tsx
@@ -1,14 +1,32 @@
-import NavbarButton from "@ui/navbar/navbar-button";
+"use client";
+
+import Container from "@ui/container/container";
+import NavbarButton, {
+  NAVBAR_BUTTON_CLASSNAME,
+} from "@ui/navbar/navbar-button";
 import Link from "next/link";
+import DarkModePopup from "@ui/button/dark-mode-popup";
 
 export default function Navbar() {
   return (
-    <nav className="flex fixed top-0 px-4 py-2 left-0 w-full bg-black/70 backdrop-blur-xl z-[1000]">
-      <div>
-        <Link href="/" passHref legacyBehavior>
-          <NavbarButton>i10e.dev</NavbarButton>
-        </Link>
-      </div>
+    <nav className="fixed top-0 px-4 py-2 left-0 w-full bg-black/70 backdrop-blur-xl z-[1000] flex justify-center">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="flex gap-2 justify-start">
+            <Link href="/" passHref legacyBehavior>
+              <NavbarButton className="font-mono">&gt; i10e.dev</NavbarButton>
+            </Link>
+            <div className="hidden lg:block">
+              <Link href={"/about"} passHref legacyBehavior>
+                <NavbarButton>About</NavbarButton>
+              </Link>
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <DarkModePopup className={NAVBAR_BUTTON_CLASSNAME} anchor={{to: "bottom end", gap: "0.25rem"}} />
+          </div>
+        </div>
+      </Container>
     </nav>
   );
 }
--- lib/components/ui/navbar/navbar-button.tsx
+++ lib/components/ui/navbar/navbar-button.tsx
@@ -3,13 +3,16 @@
 import { Button as HButton } from "@headlessui/react";
 import type { ButtonType } from "@interfaces/ui/button/button";
 
+export const NAVBAR_BUTTON_CLASSNAME =
+  "inline-block px-2 py-1 text-white bg-white/0 hover:bg-white/10 active:bg-white/25 data-[active]:bg-white/25 data-[open]:bg-white/25 transition-colors rounded-md";
+
 export default function NavbarButton({
   href,
   children,
   className,
   onClick,
 }: Readonly<ButtonType>) {
-  const classNameString = `p-2 bg-white/0 hover:bg-white/10 active:bg-white/25 transition-colors rounded-md ${className}`;
+  const classNameString = `${NAVBAR_BUTTON_CLASSNAME} ${className || ""}`;
   return href ? (
     <HButton as="a" className={classNameString} href={href} onClick={onClick}>
       {children}
