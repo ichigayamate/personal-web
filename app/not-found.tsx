6388772acd62f06e48aa755d93af0678f45d5e3a
ichigayamate <kyokodenshae353kei@gmail.com>
2024-10-29 11:45:39 +0700

    main/added auto redirect on not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 export default function NotFound() {
   const router = useRouter();
+  const pathname = usePathname();
   useEffect(() => {
+    toast.error(`Page ${pathname} not found`);
     router.replace("/");
   });
   return <></>;
